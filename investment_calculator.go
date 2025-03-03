ebf5313876bb5c59d4b25a899fad9038dfc51e07
Andres Morales <meliand11@yahoo.com>
2025-03-03 16:17:34 -0500

    final output added with variables

--- investment_calculator.go
+++ investment_calculator.go
@@ -9,20 +9,22 @@ func main() {
 	const inflationRate = 2.5
 	var investmentAmount float64
 	var years float64
-	var expectedReturnRate float64
+	expectedReturnRate := 5.5
 
 	fmt.Print("Investment Amount: ")
 	fmt.Scan(&investmentAmount)
 
-	fmt.Print("Number of years: ")
-	fmt.Scan(&years)
-
 	fmt.Print("Expected rate of return: ")
 	fmt.Scan(&expectedReturnRate)
+
+	fmt.Print("Number of years: ")
+	fmt.Scan(&years)
 
 	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
 	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
 
-	fmt.Println(futureValue)
-	fmt.Println(futureRealValue)
+	// is used to add a comment in golang
+	//fmt.Println("Future Value:", futureValue)
+	fmt.Printf("Future Value: %v\nFuture Value (adjusted for Inflation): %v", futureValue, futureRealValue)
+	//fmt.Println("Future Value (adjusted for Inflation):", futureRealValue)
 }
