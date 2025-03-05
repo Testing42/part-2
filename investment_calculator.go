cf7a6e89bb763312a4f1199ec516a65dc8437d4c
Andres Morales <meliand11@yahoo.com>
2025-03-05 12:39:35 -0500

    functionsadded

--- investment_calculator.go
+++ investment_calculator.go
@@ -5,23 +5,30 @@ import (
 	"math"
 )
 
+const inflationRate = 2.5
+
 func main() {
-	const inflationRate = 2.5
+
 	var investmentAmount float64
 	var years float64
 	expectedReturnRate := 5.5
 
-	fmt.Print("Investment Amount: ")
+	//fmt.Print("Investment Amount: ")
+	//firsttime using a function
+	outputText("Investment Amount: ")
 	fmt.Scan(&investmentAmount)
 
-	fmt.Print("Expected rate of return: ")
+	//fmt.Print("Expected rate of return: ")
+	outputText("Expected rate of return: ")
 	fmt.Scan(&expectedReturnRate)
 
-	fmt.Print("Number of years: ")
+	//fmt.Print("Number of years: ")
+	outputText("Number of years: ")
 	fmt.Scan(&years)
 
-	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
-	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
+	futureValue, futureRealValue := calculateFutureValues(investmentAmount, expectedReturnRate, years)
+	//futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
+	//futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
 
 	//if you want to format the text below as variables before printing
 	formattedFV := fmt.Sprintf("Future Value: %.2f\n", futureValue)
@@ -36,7 +43,19 @@ func main() {
 	//fmt.Println("Future Value (adjusted for Inflation):", futureRealValue)
 
 	//to have a multi line sting use ` ` to close the string example below \n isn't needed
-	fmt.Printf(`Future Value: %.2f
-	
-	Future Value (adjusted for Inflation): %.2f`, futureValue, futureRealValue)
+	//fmt.Printf(`Future Value: %.2f
+
+	//Future Value (adjusted for Inflation): %.2f`, futureValue, futureRealValue)
+}
+
+//example function
+
+func outputText(text string) {
+	fmt.Print(text)
+}
+
+func calculateFutureValues(investmentAmount, expectedReturnRate, years float64) (float64, float64) {
+	fv := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
+	rfv := fv / math.Pow(1+inflationRate/100, years)
+	return fv, rfv
 }
